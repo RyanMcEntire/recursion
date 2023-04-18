bc146ce898b9e678980eaea42e35ccbac911b20c
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2023-04-17 20:08:00 -0600

    remove extra line and comment

--- exercise-1-fibonacci.js
+++ exercise-1-fibonacci.js
@@ -13,13 +13,10 @@ console.log(fibs(8));
 // returns x digits from the fibonacci sequence
 // recursive
 function fibsRec(num, array = [0, 1]) {
-  const max = num;
   if (num > 0) {
     array = array.concat(array.slice(-1)[0] + array.slice(-2, -1)[0]);
     return fibsRec(num - 1, array);
   }
   return array.slice(0, array.length - 2);
 }
-console.log(fibsRec(7));
-
-// add last + second last to Array
+console.log(fibsRec(1));
